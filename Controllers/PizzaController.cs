117c68746eec0df20c7feea559925f43e965b945
lokeshrookie <rocklinglokesh@gmail.com>
2024-05-29 08:45:34 +0530

    Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,72 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController() { }
+
+        // GET All Action
+        [HttpGet("GetAll")]
+        public ActionResult<IEnumerable<Pizza>> GetPizzas()
+        {
+            return PizzaService.GetAll();
+        }
+
+        // GET By Id Action
+        [HttpGet("Get/{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if(pizza == null)
+            {
+                return NotFound();
+            }
+            return pizza;
+        }
+
+        // POST Action
+        [HttpPost("Create")]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
+
+        // PUT Action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id) return BadRequest();
+            
+            var existingPizza = PizzaService.Get(id);
+
+            if(existingPizza == null)
+            {
+                return NotFound();
+            }
+
+            PizzaService.Update(id, pizza);
+            return NoContent(); // return 204. means the pizza is updated.
+
+        }
+
+        // DELETE Action
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null) return NotFound();
+
+            PizzaService.Delete(id);
+            
+            return NoContent(); // 204, means pizza is deleted.
+        }
+    }
+}
---
+++ Services/PizzaService.cs
@@ -0,0 +1,84 @@
+﻿using ContosoPizza.Models;
+
+namespace ContosoPizza.Services
+{
+    public static class PizzaService
+    {
+        static List<Pizza> Pizzas { get; set; }
+        static int nextId = 3;
+
+        // Constructor
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>()
+            {
+                new Pizza()
+                {
+                    Id = 1, 
+                    Name = "Classic Italian",
+                    IsGlutenFree = false
+                },
+                new Pizza()
+                {
+                    Id = 2,
+                    Name = "Veggie",
+                    IsGlutenFree = true
+                }
+            };
+        }
+
+        //Get All
+        public static List<Pizza> GetAll()
+        {
+
+            return Pizzas;
+        }
+
+        //Get by Id
+        public static Pizza? Get(int id)
+        {
+            return Pizzas.FirstOrDefault(x => x.Id == id);
+        }
+        
+        // Add pizza
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+
+
+        //Delete
+        public static void Delete(int id)
+        {
+            Pizza item = Pizzas.FirstOrDefault(x => x.Id == id);
+            if(item is null)
+            {
+                return;
+            }
+            Pizzas.Remove(item);
+        }
+
+        //Update
+        public static void Update(int id,  Pizza pizza)
+        {
+            //// Get the pizza using id, 
+            //Pizza item = Pizzas.Find(x => x.Id == id);
+            //// update  the properties of pizza,
+            //item.Name = pizza.Name;
+            //item.IsGlutenFree = pizza.IsGlutenFree;
+
+            //or
+
+            // find the index of pizza you want to update, 
+            // replace old one with new one.
+            var index = Pizzas.FindIndex(x => x.Id == pizza.Id);
+            if(index == -1)
+            {
+                return;
+            }
+            Pizzas[index] = pizza;
+        }
+
+    }
+}
